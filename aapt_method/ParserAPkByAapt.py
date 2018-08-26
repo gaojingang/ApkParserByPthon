44587436ad1f36cbd257ee0c367b8c0a7af9a9b2
gaojingang <gaojingangzh@gmail.com>
2018-08-27 00:05:02 +0800

    1、解决GBK无法解码问题 2、修改AppName 获取方式 3、当icon为svg格式时，不支持解析

--- aapt_method/ParserAPkByAapt.py
+++ aapt_method/ParserAPkByAapt.py
@@ -1,10 +1,14 @@
-# -*- coding:utf8 -*-
+# -*- coding:utf-8 -*-
 import os
 import re
+import sys
+import subprocess
+
+
 '''
         apkInfo.name = this.regexpOne(stdout, /application-label-zh-CN:'([^']+)'/g) ||
-          this.regexpOne(stdout, /application-label-zh:'([^']+)'/g) ||
-          this.regexpOne(stdout, /label='([^']+)'/g)
+        this.regexpOne(stdout, /application-label-zh:'([^']+)'/g) ||
+        this.regexpOne(stdout, /label='([^']+)'/g)
           
         apkInfo.package = this.regexpOne(stdout, /package: name='([^']+)'/g)
         apkInfo.versionName = this.regexpOne(stdout, /versionName='([^']+)'/g)
@@ -15,27 +19,22 @@ import re
 
 def getAppBaseInfo(apkpath):
     # 检查版本号等信息
-    DELETE_PACKAGES = "android.permission.DELETE_PACKAGES"
-    INSTALL_PACKAGES = "android.permission.INSTALL_PACKAGES"
-
-    output = os.popen("aapt.exe d badging %s" % apkpath).read()
+    PERMISSION_DELETE_PACKAGES = "android.permission.DELETE_PACKAGES"
+    PERMISSION_INSTALL_PACKAGES = "android.permission.INSTALL_PACKAGES"
+    cmd = "aapt.exe d badging {}".format(apkpath)
+    #output = os.popen("aapt.exe d badging %s" % apkpath).read()
+    output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
     print (output)
 
-    m = re.compile(
-        "application-label-zh-CN:'([^']+)' | application-label-zh:'([^']+)' | label='([^']+)'").match(output)
+    lable = re.findall("application-label-zh-CN:'([^']+)'", output, re.I | re.M)[0]
+    if lable is None:
+        lable = re.findall("application-label:'([^']+)'", output, re.I | re.M)[0]
 
-    if not m:
-        print("can't get packageinfo\n\n")
-    else :
-        print (m.group())
-
-    lable = re.findall("application-label-zh-CN:'([^']+)' | application-label-zh:'([^']+)' | label='([^']+)'", output,
-                       re.I | re.M)[0][-1]
     apkName = lable
-    packagename = re.findall("package: name='([^']+)'",output ,re.M)[0]
-    versionCode = re.findall("versionCode='([^']+)'", output ,re.M)[0]
-    versionName = re.findall("versionName='([^']+)'", output ,re.M)[0]
-    minSdk = re.findall("sdkVersion:'([^']+)'", output ,re.M)[0]
+    packagename = re.findall("package: name='([^']+)'",output ,re.I | re.M)[0]
+    versionCode = re.findall("versionCode='([^']+)'", output ,re.I | re.M)[0]
+    versionName = re.findall("versionName='([^']+)'", output ,re.I | re.M)[0]
+    minSdk = re.findall("sdkVersion:'([^']+)'", output ,re.I | re.M)[0]
 
     print('apkName:' + apkName)
     print('packagename:' + packagename)
@@ -44,23 +43,47 @@ def getAppBaseInfo(apkpath):
     print('minSdk:' + minSdk)
 
     #获取icon
-    iconPath =  re.findall("icon='([^']+)'", output ,re.M)[0]
-    print('iconPath:' + iconPath)
+    iconPath = ""
+    try:
+        iconPath = re.findall("icon='([^']+)'", output, re.I | re.M)[0]
+        ext = os.path.splitext(iconPath)[-1][1:]
+        print ("ext:",ext)
+        if ext == 'xml':
+            print ("Not support svg file.")
+            iconPath = ""
+    except IndexError:
+        iconPath = ""
 
-    temPath = "temp"
-    #let command = `"${decompressFile(getUnzipFileOriginPath())}" "${apkPath}" "${getTmpPath()}" "${iconPath}"`
-    os.popen("unzip.exe {} {} {} ".format(apkpath,temPath,iconPath))
-    iconLocalPath = temPath + "/" + os.path.basename(iconPath)
-    print (iconLocalPath)
+    print("iconPath:" + iconPath)
+
+    #获取APK icon
+    if iconPath == "":
+        print ("IconPath is None")
+    else:
+        temPath = "temp"
+        #let command = `"${decompressFile(getUnzipFileOriginPath())}" "${apkPath}" "${getTmpPath()}" "${iconPath}"`
+        os.popen("unzip.exe {} {} {} ".format(apkpath,temPath,iconPath))
+        iconLocalPath = temPath + "/" + os.path.basename(iconPath)
+        print('iconLocalPath:' + iconLocalPath)
 
     # 获得所有的权限
     permissons = []
     outList = output.split('\n')
     for line in outList:
-        if line.startswith('uses-permission:'):
-            permissons.append(line.split('=')[1])
+        if line.startswith('uses-permission'):
+            per = line.split('=')[1]
+            per = per.replace("'","")
+            per = per.replace("\r", "")
+            permissons.append(per)
 
     print (permissons)
+    #判断是否存在敏感权限
+    if PERMISSION_INSTALL_PACKAGES in permissons:
+        print ("Has exist "+ PERMISSION_INSTALL_PACKAGES)
+
+    if PERMISSION_DELETE_PACKAGES in permissons:
+        print ("Has exist "+ PERMISSION_DELETE_PACKAGES)
+
 
 def checkDangerousPermission(apkpath):
     #判断是否具有危险权限
@@ -131,18 +154,11 @@ def checkDangerousPermission(apkpath):
             print("====================================\n")
 
 if __name__ == '__main__':
-    apkName = "com.google.android.packageinstaller_7.0-24.apk"
-    #apkName = "samsungbypassgoogleverify.apk"
 
-    #getAppBaseInfo(apkName)
-    checkDangerousPermission(apkName)
+    print(sys.getdefaultencoding() )
+    apkName = "testApp/com.google.android.packageinstaller_7.0-24.apk"
+    #apkName = "Facebook_v186.0.0.48.81_apkpure.com.apk"
 
-
-
-
-
-
-
-
-
+    getAppBaseInfo(apkName)
+    #checkDangerousPermission(apkName)
 
